d2076751ed293d19758b9389eae8e5a1dd01cd5f
seokyHan <sseok1979@naver.com>
2024-08-05 23:49:38 +0900

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+export default new VueRouter({
+	mode: 'history',
+	scrollBehavior() {
+		return {
+			y: 0,
+		};
+	},
+	routes: [
+		{
+			path: '/',
+			component: () => import('@/views/MainPage.vue'),
+		},
+		
+	],
+});
---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import userStore from '@/store/modules/userStore';
+import searchStore from '@/store/modules/searchStore';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+	modules: {
+		// store 등록
+	},
+});
+
+export default store;
