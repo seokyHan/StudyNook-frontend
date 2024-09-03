8c6a28f382b28b7264e50d50a779c9beb42c5d0c
seokyHan <sseok1979@naver.com>
2024-09-03 17:22:15 +0900

    add : init header setting

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,17 +1,20 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <header id="header">
-    <RouterLink to="/" class="header__title">StudyNook</RouterLink>
+    <RouterLink to="/" class="header__title"></RouterLink>
+    <HeaderNavigation />
   </header>
 </template>
 
 <script>
 import HeaderNavigation from '@/components/common/HeaderNavigation.vue';
 export default {
-  components: {},
-  methods: {
-    test() {
-      const a = '';
-    },
+  components: {
+    HeaderNavigation,
   },
 };
 </script>
+
+<style lang="css" scoped>
+@import './css/header.css';
+</style>
---
+++ src/components/common/HeaderNavigation.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="inner">
+    <a href="/" class="logo">
+      <img src="@/images/snook_logo.png" alt="SNOOK" />
+      <!-- <p>LOGO</p> -->
+    </a>
+
+    <!-- <div class="sub-menu">
+          <ul class="menu">
+            <li>
+              <a href="/signin">Sign In</a>
+            </li>
+            <li>
+              <a href="javascript:void(0)">My Starbucks</a>
+            </li>
+            <li>
+              <a href="javascript:void(0)">Customer Service & Ideas</a>
+            </li>
+            <li>
+              <a href="javascript:void(0)">Find a Store</a>
+            </li>
+          </ul>
+          <div class="search">
+            <input type="text" />
+            <span class="material-icons">search</span>
+          </div>
+        </div> -->
+    <div class="menu">
+      <ul class="main-menu">
+        <li class="item">
+          <div class="item__name">menu1</div>
+        </li>
+
+        <li class="item">
+          <div class="item__name">menu2</div>
+        </li>
+        <li class="item">
+          <div class="item__name">menu3</div>
+        </li>
+        <li class="item">
+          <div class="item__name">menu4</div>
+        </li>
+      </ul>
+      <div class="button-list">
+        <button class="fast white" v-html="formattedButtonText"></button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      buttonText: '로그인',
+    };
+  },
+  computed: {
+    formattedButtonText() {
+      return (
+        '<div><span>' +
+        this.buttonText.trim().split('').join('</span><span>') +
+        '</span></div>'
+      );
+    },
+  },
+};
+</script>
+
+<style lang="css" scoped>
+@import './css/headerNavigation.css';
+</style>
