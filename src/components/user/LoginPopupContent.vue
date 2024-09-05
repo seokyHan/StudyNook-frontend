09ae2b6d5bad7d80f5fb585112990cd234b27fd8
kimy12 <shinkim7941@gmail.com>
2024-09-05 22:34:41 +0900

    fix : 공통 모달 css 수정 및 로그인 팝업 수정

--- src/components/user/LoginPopupContent.vue
+++ src/components/user/LoginPopupContent.vue
@@ -1,9 +1,29 @@
 <template>
-  <h2>HI</h2>
+  <div class="login-box">
+    <div class="login-intro">
+      <img src="../../images/snook_logo.png" />
+    </div>
+    <div class="login-button-group">
+      <div class="kako-btn">
+        <img src="../../images/kakao.png" />
+        <div>Kakao 로그인</div>
+      </div>
+      <div class="naver-btn">
+        <img src="../../images/naver.png" />
+        <div>Naver 로그인</div>
+      </div>
+      <div class="google-btn">
+        <img src="../../images/google.png" />
+        <div>Google 로그인</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style lang="scss">
+@import './css/LoginPopup.css';
+</style>
