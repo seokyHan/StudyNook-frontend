7006de0e2031d26bc3d93ac1cfe02f0ccdb45463
seokyHan <sseok1979@naver.com>
2024-09-09 18:17:42 +0900

    add : 알림 util 템플릿 추가

--- src/utils/alertUtils.js
+++ src/utils/alertUtils.js
@@ -1,12 +1,12 @@
 import Swal from 'sweetalert2';
 
 export function showAlert(title, icon, timer) {
-	Swal.fire({
-		position: 'center',
-		icon: icon,
-		width: 350,
-		title: `<div style="font-size: 18px; font-family: 'Noto Sans KR', sans-serif;">${title}</div>`,
-		showConfirmButton: false,
-		timer: timer,
-	});
+  Swal.fire({
+    position: 'center',
+    icon: icon,
+    width: 350,
+    title: `<div style="font-size: 18px; font-family: 'Noto Sans KR', sans-serif;">${title}</div>`,
+    showConfirmButton: false,
+    timer: timer,
+  });
 }
