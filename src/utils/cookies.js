a64cc2ca73b8fd99bc6ee771324cdeaab57d27e9
seokyHan <sseok1979@naver.com>
2024-09-09 18:18:07 +0900

    add : cookie 제어 추가

---
+++ src/utils/cookies.js
@@ -0,0 +1,77 @@
+function saveisLogin(value) {
+  localStorage.setItem('isLogin', value);
+}
+
+function getIdFromCookie() {
+  return document.cookie.replace(
+    /(?:(?:^|.*;\s*)id\s*=\s*([^;]*).*$)|^.*$/,
+    '$1',
+  );
+}
+
+function getEmailFromCookie() {
+  return document.cookie.replace(
+    /(?:(?:^|.*;\s*)email\s*=\s*([^;]*).*$)|^.*$/,
+    '$1',
+  );
+}
+
+function getNicknameFromCookie() {
+  return document.cookie.replace(
+    /(?:(?:^|.*;\s*)nickName\s*=\s*([^;]*).*$)|^.*$/,
+    '$1',
+  );
+}
+
+function getIsSocialLoginFirst() {
+  return document.cookie.replace(
+    /(?:(?:^|.*;\s*)isFirst\s*=\s*([^;]*).*$)|^.*$/,
+    '$1',
+  );
+}
+
+function getSocialLogin() {
+  return document.cookie.replace(
+    /(?:(?:^|.*;\s*)socialLogin\s*=\s*([^;]*).*$)|^.*$/,
+    '$1',
+  );
+}
+
+function getIsLoginFromStorage() {
+  return localStorage.getItem('isLogin');
+}
+
+function getAcessTokenFromCookie() {
+  return document.cookie.replace(
+    /(?:(?:^|.*;\s*)accessToken\s*=\s*([^;]*).*$)|^.*$/,
+    '$1',
+  );
+}
+
+function deleteIsLogin() {
+  localStorage.removeItem('isLogin');
+}
+
+function deleteCookie(value) {
+  document.cookie = `${value}=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/;`;
+}
+
+function clearAllCookies() {
+  deleteCookie('id');
+  deleteCookie('email');
+  deleteCookie('nickname');
+}
+
+export {
+  saveisLogin,
+  getIdFromCookie,
+  getEmailFromCookie,
+  getNicknameFromCookie,
+  getIsSocialLoginFirst,
+  getSocialLogin,
+  getIsLoginFromStorage,
+  getAcessTokenFromCookie,
+  deleteIsLogin,
+  deleteCookie,
+  clearAllCookies,
+};
